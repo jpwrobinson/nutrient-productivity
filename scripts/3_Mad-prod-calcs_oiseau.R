ce97e781614080c1fa3d121b7bb5839f8129685b
James Robinson <jpwrobinson@gmail.com>
2021-09-07 09:40:14 +0100

    ulam for k mod
    Former-commit-id: 74e98ec3abc46bd3b80cf92245b17d44a7ab5feb
    Former-commit-id: b03eeab466f86f26f6842918a353c4204507badf
    Former-commit-id: 5005937fa006118d5c7e9177aa2ed962ac37048c

--- scripts/3_Mad-prod-calcs_oiseau.R
+++ scripts/3_Mad-prod-calcs_oiseau.R
@@ -1,26 +1,52 @@
 source("scripts/1_Mad-Sey-prod-prep.R")
 library(rfishprod)
 library(tidyverse)
-data(db) ## load Morais reef fish growth data
+library(rethinking)
+ctoK<-function(x){x + 273.2}
 
+## load Morais reef fish growth data
+data(db) 
+
+## Questions:
+# 1) MTE is in Kelvin, as this relates to some constants
+# 2) Mixed effect model structure - intercepts, slopes?
+# 3) K conversion equation should have a negative in the exponential?
 
 ## set fixed parameters
-T = 28 # temperature, celsius
+temp = ctoK(28) # temperature, celsius
+Kb = 8.62 * 10^-5 # Boltzmann's constant
 beta = 0.25
 Ea = 0.65
 
 ## mixed effects model for missing species / genus
-mod<-lme4::lmer(
-  log(K) ~ (1 | Species) + (1 | Family),
-  data = db
-)
+dat.list<-as.list(db)
+dat.list$Kb<-Kb
+dat.list$temp<-temp
+dat.list$LogMaxSizeTL<-log(db$MaxSizeTL)
+dat.list$logK<-log(db$K)
+dat.list$Family<-as.factor(dat.list$Family)
+
+mod<-ulam(
+  alist(
+  logK ~ dnorm(mu, sigma),
+  mu <- B1*LogMaxSizeTL + B2*1/Kb*sstmean, #+ B_fam[Family],
+  
+  c(B1, B2) ~ dnorm(0, 100),
+  # B_fam[family] ~ dnorm(0, sigmar),
+  c(sigma, sigmar) ~ dexp(1)
+  ),
+  data = dat.list,  chains=3 ,iter = 2000, warmup=500, cores=4)
 
 ## correct growth rate for temperature
-growth_cor <- function(K, Tobs, Tpred=28){
-  K = Kc - exp((Ea / kb) * (1/Tobs - 1/Tpred))
+growth_cor <- function(K, Tobs, Tpred){
+  Kc = K * exp(-(Ea / Kb) * ((1/Tobs) - (1/Tpred)))
+  return(Kc)
 }
   
-
+db$K_cor<-growth_cor(K=db$K, 
+                     Tobs=ctoK(db$sstmean), 
+                     Tpred=temp)
+plot(db$K_cor, db$K)
 
 ## productivity equation
 lplus<-linf*(1 - exp(-k*(T-t0)))
@@ -28,3 +54,6 @@ lplus<-linf*(1 - exp(-k*(T-t0)))
 
 
 head(mada.prod)
+
+
+exp(-(Ea / Kb) * ((1/a) - (1/T)))
