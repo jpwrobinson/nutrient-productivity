3a4758e9635e0c30d27c0a5058b97a38d1dda76c
James Robinson <jpwrobinson@gmail.com>
2021-09-09 17:27:29 +0100

    kmax not there yet
    Former-commit-id: d7606184413afa8a0cf12c091e51dc5a78cfc555
    Former-commit-id: cbd5b72bf4049af8083d1e07369b7ab2f9fb5df5
    Former-commit-id: f0ef4ca77c5b15a31d04fea6b2910af14bf65010

--- scripts/4_compare_productivity_method.R
+++ scripts/4_compare_productivity_method.R
@@ -3,4 +3,11 @@
 prod_mte<-read.csv("data/wcs/madagascar_potential-prod-test_mte.csv")
 prod_morais<-read.csv("data/wcs/madagascar_potential-prod-test_rfishprod.csv")
 
-plot(prod_mte$prod_day, prod_morais$prod_day)
+plot(prod_mte$prod_g_day, prod_morais$prod_day, 
+     xlab = 'MTE productivity (g per day)', ylab = 'Renato productivity (g per day)')
+
+plot(prod_mte$K, prod_morais$Kmax, 
+     xlab = 'MTE K', ylab = 'Renato Kmax')
+abline(0,1)
+
+prod_morais %>% filter(prod_day>2)
