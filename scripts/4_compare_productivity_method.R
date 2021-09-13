bcb8b5ac9a5549b38ba4f7bc829e21659a193b60
James Robinson <jpwrobinson@gmail.com>
2021-09-13 15:06:16 +0100

    fixed bug in mte conversions

--- scripts/4_compare_productivity_method.R
+++ scripts/4_compare_productivity_method.R
@@ -3,11 +3,14 @@
 prod_mte<-read.csv("data/wcs/madagascar_potential-prod-test_mte.csv")
 prod_morais<-read.csv("data/wcs/madagascar_potential-prod-test_rfishprod.csv")
 
+par(mfrow=c(1,2))
 plot(prod_mte$prod_g_day, prod_morais$prod_day, 
      xlab = 'MTE productivity (g per day)', ylab = 'Renato productivity (g per day)')
+abline(0, 1)
+title('Daily productivity (grams)')
 
 plot(prod_mte$K, prod_morais$Kmax, 
      xlab = 'MTE K', ylab = 'Renato Kmax')
 abline(0,1)
+title('K from MTE vs K from Morais')
 
-prod_morais %>% filter(prod_day>2)
