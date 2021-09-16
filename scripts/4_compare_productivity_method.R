06ac816521c811b1c2594e63b2402cc6757880c9
James Robinson <jpwrobinson@gmail.com>
2021-09-16 12:02:40 +0100

    species kmax and nuts
    Former-commit-id: 80338f36897151d789648f082b91365afea9b929
    Former-commit-id: 019805d59623fbc607f0efb13e12cf2f0f02e24d
    Former-commit-id: 2e4e007b9b2827a3b559fb085f74f806630d85f9

--- scripts/4_compare_productivity_method.R
+++ scripts/4_compare_productivity_method.R
@@ -3,6 +3,7 @@
 prod_mte<-read.csv("data/wcs/madagascar_potential-prod-test_mte.csv")
 prod_morais<-read.csv("data/wcs/madagascar_potential-prod-test_rfishprod.csv")
 
+pdf(file = 'fig/mte_vs_morais/mad_daily_productivity.pdf', height = 6, width=10)
 par(mfrow=c(1,2))
 plot(prod_mte$prod_g_day, prod_morais$prod_day, 
      xlab = 'MTE productivity (g per day)', ylab = 'Renato productivity (g per day)')
@@ -12,5 +13,6 @@ title('Daily productivity (grams)')
 plot(prod_mte$K, prod_morais$Kmax, 
      xlab = 'MTE K', ylab = 'Renato Kmax')
 abline(0,1)
-title('K from MTE vs K from Morais')
+title('K from MTE vs Kmax from Morais')
 
+dev.off()
---
+++ scripts/7_nutrient_K_species.R
@@ -0,0 +1,33 @@
+source('scripts/0_plot_theme.R')
+
+## Script examining nutrient density and K of reef fishery target species
+nut<-sey_species_nut %>% select(species, ends_with('mu'), -Protein_mu) %>% 
+      mutate(nut_density = rowSums(across(c(Selenium_mu:Vitamin_A_mu))))
+
+# join nutrient concentrations with K estimate
+mad<-read.csv("data/wcs/madagascar_potential-prod-test_rfishprod.csv") %>% 
+  distinct(species, Kmax)
+
+nut<-left_join(nut, mad, by = 'species') %>% droplevels()
+
+ggplot(nut, aes(Kmax, nut_density)) + geom_point() +
+      geom_label(data = nut %>% filter(nut_density>400), aes(label = species)) +
+      geom_label(data = nut %>% filter(Kmax>0.4), aes(label = species))
+
+g2<-ggplot(nut, aes(fct_reorder(species, Kmax), Kmax, col=nut_density)) + 
+      geom_point() +
+      coord_flip() +
+      labs(x='', y = 'Kmax')
+
+g3<-ggplot(nut, aes(fct_reorder(species, nut_density), nut_density, col=Kmax)) + 
+  geom_point() +
+  coord_flip() +
+  labs(x='', y = 'Nutrient density, %')
+
+pdf(file = 'fig/explore/species_Kmax.pdf', height=12, width = 5)
+g2
+dev.off()
+
+pdf(file = 'fig/explore/species_nutrient_density.pdf', height=12, width = 5)
+g3
+dev.off()
