e9bb4efa35eae395e7dee6007f33bf616eb2ef30
James Robinson <jpwrobinson@gmail.com>
2021-09-16 15:45:01 +0100

    fig redo with complete()
    Former-commit-id: 2b0292fc82b5571e315c0063fc6444a5e75ee184
    Former-commit-id: 0870abe6ac7fcde159d619158a07a707d24ee289
    Former-commit-id: bebdb12aeda8e038de8f0db8e4f325e966a38799

--- scripts/6_nut_productivity_mad.R
+++ scripts/6_nut_productivity_mad.R
@@ -19,7 +19,17 @@ mad_reef<-mad %>% group_by(country, management, site, sample_date, transect_numb
             prod_day = sum(prod_day),
             biomass_g = sum(mass)) %>% 
           mutate(nut_turnover = nut_prod_day / nut_biomass,
-                 biomass_turnover = prod_day / biomass_g)
+                 biomass_turnover = prod_day / biomass_g) %>% 
+          select(country, site, species, nutrient, trophic_group, nut_prod_day:biomass_g) %>% 
+          complete(site, nutrient, nesting(species, trophic_group, country),
+                   fill = list(nut_prod_day = 0, nut_biomass = 0, prod_day =0, biomass_g = 0, biomass_turnover=0, nut_turnover=0)) %>%
+          group_by(country,site,  species, nutrient, trophic_group) %>% 
+          summarise(nut_prod_day = mean(nut_prod_day), 
+                    nut_biomass = mean(nut_biomass),
+                    prod_day = mean(prod_day),
+                    biomass_g = mean(biomass_g),
+                    nut_turnover=mean(nut_turnover),
+                    biomass_turnover = mean(biomass_turnover))
 
 pdf(file = 'fig/explore/mad_nutrient_prod_reef.pdf', height=7, width=12)
 ggplot(mad_reef, aes(biomass_g, nut_prod_day)) + geom_point() + facet_wrap(~nutrient, scales='free')
@@ -31,14 +41,26 @@ ggplot(mad_reef, aes(biomass_turnover, nut_turnover)) + geom_point() + facet_wra
 dev.off()
 
 ## species level estimates of nut prod metrics
-mad_sp<-mad %>% group_by(country, management, site, sample_date, species, transect_number, depth, nutrient, trophic_group) %>% 
+mad_sp<-mad %>% 
+  group_by(country, management, site, sample_date, species, transect_number, depth, nutrient, trophic_group) %>% 
   summarise(
     nut_prod_day = sum(nut_prod_day), 
     nut_biomass = sum(nut_biomass),
     prod_day = sum(prod_day),
     biomass_g = sum(mass)) %>% 
-  mutate(nut_turnover = nut_prod_day / nut_biomass,
-         biomass_turnover = prod_day / biomass_g) %>% 
+  ungroup() %>% 
+  group_by(country, management, site, species, nutrient, trophic_group) %>% 
+  summarise(
+    nut_prod_day = mean(nut_prod_day), 
+    nut_biomass = mean(nut_biomass),
+    prod_day = mean(prod_day),
+    biomass_g = mean(biomass_g)) %>% 
+  ungroup() %>% 
+  select(country, site, species, nutrient, trophic_group, nut_prod_day:biomass_g) %>% 
+  complete(site, nutrient, nesting(species, trophic_group, country),
+           fill = list(nut_prod_day = 0, nut_biomass = 0, prod_day =0, biomass_g = 0)) %>%
+  # mutate(nut_turnover = nut_prod_day / nut_biomass,
+  #        biomass_turnover = prod_day / biomass_g) %>% 
   group_by(country, species, nutrient, trophic_group) %>% 
   summarise(nut_prod_day = mean(nut_prod_day), 
             nut_biomass = mean(nut_biomass),
@@ -52,7 +74,8 @@ mad_top_nut<-mad_sp %>% ungroup() %>% group_by(nutrient) %>% slice_max(nut_prod_
 mad_scale<-mad_sp %>% group_by(nutrient) %>% 
       mutate(nut_prod_day_scale = scale(nut_prod_day)) %>% 
       ungroup() %>% group_by(species, trophic_group) %>% 
-      summarise(nut_prod_score = sum(nut_prod_day_scale))
+      summarise(nut_prod_score = sum(nut_prod_day_scale),
+                biomass_g = unique(biomass_g))
 
 mad_scale$trophic_lab<-trophic.cols$FG_lab[match(mad_scale$trophic_group, trophic.cols$FG)]
 
@@ -60,6 +83,10 @@ pdf(file = 'fig/explore/mad_nutrient_prod_species.pdf', height=7, width=12)
 ggplot(mad_top_nut, aes(species, nut_prod_day)) + 
     geom_bar(stat='identity') + coord_flip() + 
     facet_wrap(~nutrient, scales='free')
+
+ggplot(mad_scale, aes(log10(biomass_g), nut_prod_score)) + 
+  geom_point(aes(col = trophic_lab)) +
+  scale_fill_manual(values = trophic_cols.named) 
 dev.off()
 
 pdf(file = 'fig/explore/mad_nutrient_prod_species_score.pdf', height=14, width=8)
