d46b87e88f9ab6094aa1e7c506a01e3a5e6aa01b
James Robinson <jpwrobinson@gmail.com>
2021-09-21 13:26:24 +0100

    fig2 and s1
    Former-commit-id: 136c93645517e8d064158ba2c29809b7da8fb19b
    Former-commit-id: c554d78830dc1c675ec5407120d65fa80baa3dd2

--- scripts/6_nut_productivity_mad.R
+++ scripts/6_nut_productivity_mad.R
@@ -1,35 +1,41 @@
 source('scripts/5_add_nutrients.R')
-nut<-sey_species_nut %>% select(species, ends_with('mu'), -Protein_mu)
+nut<-sey_species_nut %>% 
+        select(species, ends_with('mu'), -Protein_mu) %>% 
+        rename_at(vars(ends_with('mu')), ~str_replace_all(.x, '_mu', ''))
 
 # join mean nutrient concentrations with productivity estimates
 ## pivot and estimate nutrient productivity per nutrient
 # definitions: https://github.com/jpwrobinson/nut-prod/issues/1
 mad<-read.csv("data/wcs/madagascar_potential-prod-test_rfishprod.csv") %>% 
     left_join(nut, by = 'species') %>% 
-    pivot_longer(Selenium_mu:Vitamin_A_mu, names_to = 'nutrient', values_to = 'conc') %>% 
+    pivot_longer(Selenium:Vitamin_A, names_to = 'nutrient', values_to = 'conc') %>% 
     mutate(mass = a * size^b,
            nut_prod_day = conc / 100 * prod_day * 0.87,
            nut_biomass = conc * 0.87 * mass)
 
 ## reef level estimates of nutrient productivity metrics
-mad_reef<-mad %>% group_by(country, management, site, sample_date, transect_number, depth, nutrient) %>% 
+mad_reef<-mad %>% group_by(country, species, trophic_group, management, site, sample_date, transect_number, depth, nutrient) %>% 
           summarise(
             nut_prod_day = sum(nut_prod_day), 
             nut_biomass = sum(nut_biomass),
             prod_day = sum(prod_day),
             biomass_g = sum(mass)) %>% 
+          group_by(country, species, trophic_group, management, site, depth, nutrient) %>% 
+          summarise(
+            nut_prod_day = mean(nut_prod_day), 
+            nut_biomass = mean(nut_biomass),
+            prod_day = mean(prod_day),
+            biomass_g = mean(biomass_g)) %>% 
+          ungroup() %>% 
           mutate(nut_turnover = nut_prod_day / nut_biomass,
                  biomass_turnover = prod_day / biomass_g) %>% 
-          select(country, site, species, nutrient, trophic_group, nut_prod_day:biomass_g) %>% 
-          complete(site, nutrient, nesting(species, trophic_group, country),
-                   fill = list(nut_prod_day = 0, nut_biomass = 0, prod_day =0, biomass_g = 0, biomass_turnover=0, nut_turnover=0)) %>%
-          group_by(country,site,  species, nutrient, trophic_group) %>% 
-          summarise(nut_prod_day = mean(nut_prod_day), 
-                    nut_biomass = mean(nut_biomass),
-                    prod_day = mean(prod_day),
-                    biomass_g = mean(biomass_g),
-                    nut_turnover=mean(nut_turnover),
-                    biomass_turnover = mean(biomass_turnover))
+          group_by(country,site, nutrient) %>% 
+          summarise(nut_prod_day = sum(nut_prod_day), 
+                    nut_biomass = sum(nut_biomass),
+                    prod_day = sum(prod_day),
+                    biomass_g = sum(biomass_g),
+                    nut_turnover=sum(nut_turnover),
+                    biomass_turnover = sum(biomass_turnover))
 
 pdf(file = 'fig/explore/mad_nutrient_prod_reef.pdf', height=7, width=12)
 ggplot(mad_reef, aes(biomass_g, nut_prod_day)) + geom_point() + facet_wrap(~nutrient, scales='free')
