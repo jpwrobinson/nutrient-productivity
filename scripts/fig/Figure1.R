c783c70279a0388f4d23fc9456c11a1eb9bf5bd3
James Robinson <jpwrobinson@gmail.com>
2021-09-21 13:26:24 +0100

    fig2 and s1
    Former-commit-id: 136c93645517e8d064158ba2c29809b7da8fb19b
    Former-commit-id: c554d78830dc1c675ec5407120d65fa80baa3dd2
    Former-commit-id: d46b87e88f9ab6094aa1e7c506a01e3a5e6aa01b

--- scripts/fig/Figure1.R
+++ scripts/fig/Figure1.R
@@ -2,7 +2,8 @@ source('scripts/0_plot_theme.R')
 set.seed(47)
 ## Script examining nutrient density and K of reef fishery target species
 nut<-sey_species_nut %>% select(species, ends_with('mu'), -Protein_mu) %>% 
-  mutate(nut_density = rowSums(across(c(Selenium_mu:Vitamin_A_mu))))
+  mutate(nut_density = rowSums(across(c(Selenium_mu:Vitamin_A_mu)))) %>% 
+  rename_at(vars(ends_with('mu')), ~str_replace_all(.x, '_mu', ''))
 
 # join nutrient concentrations with K estimate
 mad<-read.csv("data/wcs/madagascar_potential-prod-test_rfishprod.csv") %>% 
@@ -16,6 +17,8 @@ sp<-str_split_fixed(nut$species, '\\ ', 2)
 # nut$species.lab<-paste0(substring(sp[,1],1,5),  '. ', sp[,2], '.')
 nut$species.lab<-paste0(sp[,1],'\n', sp[,2])
 
+# long version
+nutl <- nut %>% pivot_longer(Selenium:Vitamin_A, names_to = 'nutrient', values_to = 'conc')
 
 gg<-ggplot(nut, aes(Kmax, nut_density, col=trophic_lab)) + 
   geom_point(size=4, pch=21, col='black', aes(fill=trophic_lab)) +
@@ -28,12 +31,21 @@ gg<-ggplot(nut, aes(Kmax, nut_density, col=trophic_lab)) +
   scale_fill_manual(values = trophic_cols.named) +
   scale_colour_manual(values = trophic_cols.named)
 
-# load('results/madagascar_nut_prod.rds')
-# mad_sp$Kmax<-nut$Kmax[match(mad_sp$species, nut$species)]
-# mad_sp$nut_density<-nut$nut_density[match(mad_sp$species, nut$species)]
-# ggplot(mad_sp, aes(log10(biomass_g), Kmax)) + geom_point()
-# ggplot(mad_sp, aes(log10(biomass_g), nut_density)) + geom_point()
+gpan<-ggplot(nutl, aes(Kmax, conc, col=trophic_lab)) +
+  geom_point(size=4, pch=21, col='black', aes(fill=trophic_lab)) +
+  labs(x = 'Growth coefficent (Kmax)', y = expression(paste0('Nutrient concentration 100g'^'-1'))) +
+  scale_x_continuous(breaks=seq(0, 0.6, by = 0.1)) +
+  th +
+  facet_wrap(~nutrient, scales='free', nrow=5) +
+  theme(legend.position = 'none') +
+  scale_fill_manual(values = trophic_cols.named) +
+  scale_colour_manual(values = trophic_cols.named)
+
 
 pdf(file = 'fig/Figure1.pdf', height=6, width=8)
 gg
+dev.off()
+
+pdf(file = 'fig/FigureS1.pdf', height=6, width=8)
+gpan
 dev.off()
---
+++ scripts/fig/Figure2.R
@@ -0,0 +1,55 @@
+
+## FG level nutrient productivity
+source('scripts/0_plot_theme.R')
+
+load(file = 'results/madagascar_nut_prod.rds')
+mad_sp$trophic_lab<-trophic.cols$FG_lab[match(mad_sp$trophic_group, trophic.cols$FG)]
+
+mad_fg<-mad_sp %>% group_by(trophic_lab,nutrient) %>% 
+                      summarise(
+                        nut_prod_day = sum(nut_prod_day),
+                        prod_day = sum(prod_day),
+                        biomass_g = sum(biomass_g)) %>% 
+                  group_by(nutrient) %>% 
+                  mutate(nut_prod_day_scaled = scale(nut_prod_day))
+
+mad_fg2<-mad_fg %>% group_by(trophic_lab) %>% 
+  summarise(
+    nut_prod_day = mean(nut_prod_day_scaled),
+    prod_day = unique(prod_day),
+    biomass_g = unique(biomass_g)) 
+
+ggplot(mad_fg, aes(nutrient, nut_prod_day, fill=trophic_lab)) + 
+      geom_bar(stat='identity') +
+      coord_flip() +
+      theme(legend.position = 'none') +
+      scale_fill_manual(values = trophic_cols.named) +
+      scale_color_manual(values = 'white') +
+      facet_wrap(~nutrient, scales='free', nrow=6)
+
+ggplot(mad_sp, aes(trophic_lab, nut_prod_day, fill=species)) + 
+  geom_bar(stat='identity') +
+  coord_flip() +
+  theme(legend.position = 'none') +
+  # scale_fill_manual(values = trophic_cols.named) +
+  scale_color_manual(values = 'white') +
+  facet_wrap(~nutrient, scales='free', nrow=6)
+
+ggplot(mad_fg, aes(scale(prod_day), 
+                    nut_prod_day_scaled, col=trophic_lab)) + 
+          geom_point() + geom_abline(intercept=0, slope=1 ,col='grey', linetype=5) +
+          # geom_smooth(method='lm', se = FALSE) +
+          scale_colour_manual(values = trophic_cols.named)
+  
+g1<-ggplot(mad_fg2, aes(fct_reorder(trophic_lab, nut_prod_day),nut_prod_day, fill=trophic_lab),col='black') + 
+  geom_segment(aes(x = fct_reorder(trophic_lab, nut_prod_day), xend =fct_reorder(trophic_lab, nut_prod_day), y =-Inf, yend = nut_prod_day), col='grey') +
+  geom_point(size=5, pch=21) +
+  coord_flip() +
+  th + theme(legend.position = 'none') +
+  scale_fill_manual(values = trophic_cols.named)  +
+  scale_color_manual(values = trophic_cols.named) +
+  labs(x = '', y = "Mean nutrient productivity (scaled across 6 nutrients)") 
+
+pdf(file='fig/Figure2.pdf', height=6, width=9)
+g1
+dev.off()
