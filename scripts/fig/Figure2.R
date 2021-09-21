09955e37178cb00dbc96a53d4b4d37abfa606d61
James Robinson <jpwrobinson@gmail.com>
2021-09-21 16:31:15 +0100

    fig2 proportional

--- scripts/fig/Figure2.R
+++ scripts/fig/Figure2.R
@@ -15,7 +15,13 @@ mad_fg<-mad_sp %>% group_by(trophic_lab,nutrient) %>%
                         prod_day = sum(prod_day),
                         biomass_g = sum(biomass_g)) %>% 
                   group_by(nutrient) %>% 
-                  mutate(nut_prod_day_scaled = scale(nut_prod_day))
+                  mutate(nut_prod_day_scaled = scale(nut_prod_day)) %>% 
+                  arrange((nut_prod_day))  %>% 
+                  mutate(tnut = sum(nut_prod_day), nutprop = nut_prod_day / tnut * 100)
+
+## average across nutrients for panel A
+mad_fg2<-mad_fg %>% group_by(trophic_lab) %>% summarise(se = se(nutprop), nutprop = mean(nutprop)) %>% 
+        mutate(lower = nutprop - 2*se, upper = nutprop + 2*se)
 
 ggplot(mad_fg, aes(nutrient, nut_prod_day, fill=trophic_lab)) + 
       geom_bar(stat='identity') +
@@ -31,14 +37,15 @@ ggplot(mad_fg, aes(scale(prod_day),
           # geom_smooth(method='lm', se = FALSE) +
           scale_colour_manual(values = trophic_cols.named)
   
-g1<-ggplot(mad_fg2, aes(fct_reorder(trophic_lab, nut_prod_day),nut_prod_day, fill=trophic_lab),col='black') + 
-  geom_segment(aes(x = fct_reorder(trophic_lab, nut_prod_day), xend =fct_reorder(trophic_lab, nut_prod_day), y =-Inf, yend = nut_prod_day), col='grey') +
-  geom_point(size=5, pch=21) +
+g1<-ggplot(mad_fg, aes(fct_reorder(trophic_lab, nutprop),nutprop, fill=trophic_lab),col='black') + 
+  # geom_segment(aes(x = fct_reorder(trophic_lab, nut_prod_day), xend =fct_reorder(trophic_lab, nut_prod_day), y =-Inf, yend = nut_prod_day), col='grey') +
+  geom_jitter(size=2, pch=21) +
+  geom_pointrange(data = mad_fg2, aes(ymin = lower, ymax = upper), size=1.5, pch=21) +
   coord_flip() +
   th + theme(legend.position = 'none') +
   scale_fill_manual(values = trophic_cols.named)  +
   scale_color_manual(values = trophic_cols.named) +
-  labs(x = '', y = "Mean nutrient productivity (scaled across 6 nutrients)") 
+  labs(x = '', y = "Mean proportion of nutrient productivity") 
 
 g2<-ggplot(mad_sp, aes(log10(biomass_g), nutprop, col=trophic_lab)) + 
   geom_point(alpha=0.8, size=2) + facet_wrap(~nutrient, scales='free') +
