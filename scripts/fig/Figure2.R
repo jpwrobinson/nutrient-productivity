64e62b2dc82013d2fbf4faba273c4906e1e0ad84
James Robinson <jpwrobinson@gmail.com>
2021-09-21 16:16:19 +0100

    fig 2 idea
    Former-commit-id: 93c422e20471b4bc8dbf4d1ad930536244392153
    Former-commit-id: 021c28b7b2d51e3569c56bea4646cf1b9f77c896

--- scripts/fig/Figure2.R
+++ scripts/fig/Figure2.R
@@ -5,6 +5,10 @@ source('scripts/0_plot_theme.R')
 load(file = 'results/madagascar_nut_prod.rds')
 mad_sp$trophic_lab<-trophic.cols$FG_lab[match(mad_sp$trophic_group, trophic.cols$FG)]
 
+mad_sp<-mad_sp %>% group_by(nutrient) %>% 
+            arrange((nut_prod_day))  %>% 
+            mutate(tnut = sum(nut_prod_day), nutprop = nut_prod_day / tnut * 100)
+
 mad_fg<-mad_sp %>% group_by(trophic_lab,nutrient) %>% 
                       summarise(
                         nut_prod_day = sum(nut_prod_day),
@@ -13,12 +17,6 @@ mad_fg<-mad_sp %>% group_by(trophic_lab,nutrient) %>%
                   group_by(nutrient) %>% 
                   mutate(nut_prod_day_scaled = scale(nut_prod_day))
 
-mad_fg2<-mad_fg %>% group_by(trophic_lab) %>% 
-  summarise(
-    nut_prod_day = mean(nut_prod_day_scaled),
-    prod_day = unique(prod_day),
-    biomass_g = unique(biomass_g)) 
-
 ggplot(mad_fg, aes(nutrient, nut_prod_day, fill=trophic_lab)) + 
       geom_bar(stat='identity') +
       coord_flip() +
@@ -26,14 +24,6 @@ ggplot(mad_fg, aes(nutrient, nut_prod_day, fill=trophic_lab)) +
       scale_fill_manual(values = trophic_cols.named) +
       scale_color_manual(values = 'white') +
       facet_wrap(~nutrient, scales='free', nrow=6)
-
-ggplot(mad_sp, aes(trophic_lab, nut_prod_day, fill=species)) + 
-  geom_bar(stat='identity') +
-  coord_flip() +
-  theme(legend.position = 'none') +
-  # scale_fill_manual(values = trophic_cols.named) +  
-  scale_color_manual(values = 'white') +
-  facet_wrap(~nutrient, scales='free', nrow=6)
 
 ggplot(mad_fg, aes(scale(prod_day), 
                     nut_prod_day_scaled, col=trophic_lab)) + 
@@ -50,6 +40,15 @@ g1<-ggplot(mad_fg2, aes(fct_reorder(trophic_lab, nut_prod_day),nut_prod_day, fil
   scale_color_manual(values = trophic_cols.named) +
   labs(x = '', y = "Mean nutrient productivity (scaled across 6 nutrients)") 
 
-pdf(file='fig/Figure2.pdf', height=6, width=9)
-g1
+g2<-ggplot(mad_sp, aes(log10(biomass_g), nutprop, col=trophic_lab)) + 
+  geom_point(alpha=0.8, size=2) + facet_wrap(~nutrient, scales='free') +
+  scale_color_manual(values = trophic_cols.named) +
+  labs(x = 'Log10 biomass, g', y = 'Proportion of total nutrient productivity, %') +
+  th + theme(legend.position = 'none')
+
+
+pdf(file='fig/Figure2.pdf', height=6, width=14)
+print(
+  plot_grid(g1, g2, nrow=1, labels=c('A', 'B'), rel_widths=c(0.7, 1))
+)
 dev.off()
