f0bfee133cc342a7734ef0f64e8c0c96fb14d48b
EvaMaire <emg.maire@gmail.com>
2021-10-12 12:43:54 +0200

    update testing area

--- scripts/trait/extract_traits_fishbase.R
+++ scripts/trait/extract_traits_fishbase.R
@@ -11,35 +11,45 @@ require(xlsx)
 #Load functions 
 source("scripts/trait/traits_from_fishbase_functions.R")
 
-#Load GASPAR database
-gaspar <- read.csv("data/trait/gaspar.csv")
-
 #Get fish Taxonomy from Fishbase 
 tax <- load_taxa() 
 
-#Import species list
-#example with Seychelles' list
-sey <- read.csv("/Users/maire/Downloads/Species traits_Seychelles_exactMaxSizeTL.csv")
-species_list <- sey$species
+#Import species list here
+sp <- read.csv("data/trait/all_species.csv")
+
+species_list <- sp$species
 sp_data <- getTaxo(sp = species_list , tax = tax)
 
 #Test Lmax
-#lmax <- getLmax(sp_data)
-#lmax
+lmax <- getLmax(sp_data)
+head(lmax)
+summary(lmax)
+
+########################################
+#Testing area for Reef position with Seychelles data
+sey <- read.csv("/Users/maire/Downloads/Species traits_Seychelles_exactMaxSizeTL.csv")
+
+species_list <- sey$species
+
+#Load GASPAR database
+gaspar <- read.csv("data/trait/gaspar.csv")
 
 #Test MainFood
-#mainfood <- getMainFood(sp_data,Diet, Food)
-#mainfood
+Diet <- read.xlsx("data/trait/SAU data/FeedingPathway_Fishbase.xlsx",sheetName = "Diet")
+Food <- read.xlsx("data/trait/FeedingPathway_Fishbase.xlsx",sheetName = "Food Items")
+
+mainfood <- getMainFood(sp_data,Diet, Food)
+mainfood
 
 #Test Demersal versus Pelagic
-#dem <- getDemersPelag(sp_data)
-#dem
+dem <- getDemersPelag(sp_data)
+dem
 
 #Test biology
 vert <- getVerticalPosition(sp_data,gaspar)
 vert
 
-#
+#END
 
 
 
