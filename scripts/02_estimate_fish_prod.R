346560ca395d55ed8601afc54c56baa82d5ae253
James Robinson <jpwrobinson@gmail.com>
2021-11-01 15:19:38 +0000

    data and prod estimates updated per hectare
    Former-commit-id: 9a19a4a1150e7ccf6c7c5ee1b0ab65793abc8fc9
    Former-commit-id: d51ff51810917bdb30272ffd47d382f7b7b3ee17

--- scripts/02_estimate_fish_prod.R
+++ scripts/02_estimate_fish_prod.R
@@ -137,7 +137,8 @@ fishp$size_nextday<-lplus(lmax = fishp$lmax, K = fishp$Kmax, age = fishp$age )
 fishp<-fishp %>% mutate(
         prod_cm_day_perfish = size_nextday - size2,
         prod_g_day_perfish = (biomass_constant_a * size_nextday ^ biomass_constant_b) - mass,
-        prod_g_day = prod_g_day_perfish * count)
+        prod_g_day = prod_g_day_perfish * count,
+        prod_g_day_ha = prod_g_day * (10000 / transect_area)) ## convert transect to hectare
 
 with(fishp, plot(prod_cm_day_perfish, prod_g_day_perfish))
 with(fishp, plot(log10(prod_cm_day_perfish), log10(prod_g_day_perfish)))
