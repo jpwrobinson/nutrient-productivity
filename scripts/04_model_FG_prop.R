4843234d958d69dd83daa5af8cb381c0eb070352
James Robinson <jpwrobinson@gmail.com>
2021-11-08 15:34:54 +0000

    starting model script

--- scripts/04_model_FG_prop.R
+++ scripts/04_model_FG_prop.R
@@ -3,114 +3,82 @@ pacman::p_load(tidyverse, skimr, cowplot, here, funk,disco, patchwork, rethinkin
 # Load reef pressure data
 # https://github.com/WCS-Marine/local-reef-pressures
 # devtools::load_all('../local-reef-pressures') ## fails
-threat<-read.csv('data/wcs/wcs_threat_indicators.csv') %>% select(-X)
+threat<-read.csv('data/wcs/wcs_threat_indicators.csv')  %>% mutate(nutrient_load = nutrient) %>%  select(-X, -country, -nutrient)
 
 ## load fish nut prod, estimate proportional by FG
 load(file = 'results/wcs_productivity.rds')
 load(file = 'results/wcs_nut_prod.rds')
 
-focal<-prod %>% 
-  mutate(id = paste(site, year, sep = '_')) %>% 
-  ## transect level: total metric by diet group
-  group_by(country, site, id, transect_number, nutrient, dietP) %>% 
-  summarise(
-    nut_prod_day_ha = sum(nut_prod_day_ha), 
-    nut_biomass_kgha = sum(nut_biomass_kgha),
-    prod_g_day_ha = sum(prod_g_day_ha),
-    biomass_kgha = sum(biomass_kgha)) %>% 
-  ungroup() %>% 
-  ## site level: complete missing transect obs by diet group, then average
-  dplyr::select(country, site,id, transect_number, nutrient, dietP, nut_prod_day_ha:biomass_kgha) %>% 
-  group_by(nutrient, country, id, site) %>% 
-  complete(transect_number,dietP,
-           fill = list(nut_prod_day_ha = 0, nut_biomass_kgha = 0, prod_g_day_ha =0, biomass_kgha = 0)) %>% 
-  group_by(country, site, nutrient, dietP) %>% 
-  summarise(
-    nut_prod_day_ha = mean(nut_prod_day_ha), 
-    nut_biomass_kgha = mean(nut_biomass_kgha),
-    prod_g_day_ha = mean(prod_g_day_ha),
-    biomass_kgha = mean(biomass_kgha)) %>% 
-    group_by(nutrient) %>% 
-    complete(site,dietP,
-             fill = list(nut_prod_day_ha = 0, nut_biomass_kgha = 0, prod_g_day_ha =0, biomass_kgha = 0)) 
-
-
-## drop periods from names
-fish$country.x<-NULL
-fish$latitude.x<-NULL
-fish$longitude.x<-NULL
-
-sw<-unique(fish$site[fish$latitude  < -20])
-ne<-unique(fish$site[fish$latitude  > -18 & fish$latitude  < -14.1])
-nw<-unique(fish$site[fish$latitude  > -15 ])
-
-fish$seascape<-'Northwest'
-fish$seascape[fish$site %in% sw]<-'Southwest'
-fish$seascape[fish$site %in% ne]<-'Northeast'
-
-## vector of seascapes for each site, and countries for each seascape
+prod_fg<-prod_fg %>% 
+  mutate(nutrient_lab = recode(nutrient, 'calcium.mg' = 'Calcium', 'iron.mg' = 'Iron', 'zinc.mg' = 'Zinc',
+                               'selenium.mug' = 'Selenium', 'vitamin_a.mug' = 'Vitamin A', 'omega3.g' = 'Omega-3\nfatty acids')) %>% 
+  group_by(country, nutrient,nutrient_lab) %>% 
+  mutate(nut_prod_day_ha_scaled = scale(nut_prod_day_ha)[,1]) %>% 
+  arrange((nut_prod_day_ha))  %>% 
+  mutate(tnut = sum(nut_prod_day_ha), 
+         nutprop = nut_prod_day_ha / tnut * 100) 
 
 ## set up model data
+
+# read benthic
+load('data/wcs/wcs_fish_benthic.rds')
+focal<-left_join(prod_fg, 
+                fish_avg %>% ungroup() %>%  select(site, reef_type, reef_zone, management_rules, hard_coral, macroalgae, turf_algae, bare_substrate, 
+                                                   depth, fish_richness),
+                by='site') %>% 
+        filter(!is.na(depth)) %>%  # dropping 2 sites (NK02 in Madasgascar and WaiE1 in Fiji)
+        left_join(threat, by = 'site') %>% ungroup() 
+
 ## convert data to list, characters to factors for rethinking
-fish<-droplevels(fish %>% mutate_if(is.character, as.factor)) %>% select(
-  biomass_kgha_avg, country, site, seascape, hard_coral, macroalgae, reef_type, reef_zone, reef_exposure, depth_avg, we, grav_NC, management_rules, year_fish, sediment, nutrient, npp
-) 
+focal<-droplevels(focal %>% mutate_if(is.character, as.factor)) %>% select(
+  nutprop, nutrient, nutrient_lab, country, site, dietP, hard_coral, macroalgae, turf_algae, bare_substrate, reef_type, reef_zone, depth,
+  grav_NC, management_rules, sediment, nutrient_load, pop_count) 
 
 ## scale numeric
-focal.list<-scaler(fish, ID = c('biomass_kgha_avg', 'country', 'site', 'seascape', 'reef_type', 'reef_zone','reef_exposure', 'management_rules', 'year_fish'), cats = FALSE) %>% as.list()
+focal.list<-scaler(focal %>% filter(nutrient=='calcium.mg'), ID = c('nutprop', 'country', 'site', 'dietP', 'reef_type', 'reef_zone', 'management_rules'), cats = FALSE) %>% as.list()
 
 str(focal.list)
 # add list of region ID for each country name
-focal.list$seascapeLookup<-fish %>% distinct(site, seascape) %>% pull(seascape) %>% as.factor()
-focal.list$countryLookup<-fish %>% distinct(seascape, country) %>% pull(country) %>% as.factor()
-focal.list$year_fish<-as.factor(focal.list$year_fish)
+focal.list$countryLookup<-focal %>% distinct(site, country) %>% pull(country) %>% as.factor()
 
 ## check reponse hist
-hist(focal.list$biomass_kgha_avg)
-hist(log(focal.list$biomass_kgha_avg))
+hist(focal.list$nutprop)
+hist(log10(focal.list$nutprop))
 
-m_biom <- ulam(
+
+m <- ulam(
   alist(
-    biomass_kgha_avg ~ dlnorm(mu, sigma) ,
-    # biomass_kgha_avg ~ dgamma2(mu, scale) ,
+    nutprop ~ dlnorm(mu, sigma) ,
+    # nutprop ~ dgamma2(mu, scale) ,
     
     ## mu (biomass) from seascape-level intercepts and site-level covariates
-    log(mu)<- seascape_X[seascape] + 
-      coral*hard_coral + ma*macroalgae +  ## benthic
+    log(mu)<- site_X[site] + 
+      coral*hard_coral + ma*macroalgae + turf*turf_algae +  ## benthic
       # reef_ex[reef_exposure] + reef_ty[reef_type] + reef_zo[reef_zone] + 
-      depth*depth_avg + wave*we + ## biophysical small-scale
-      sed*sediment + nutr*nutrient + productivity*npp +# temperature*temp + ## biophysical large-scale,
-      management[management_rules] + gravity*grav_NC + ## human
-      year[year_fish], ## confounding / correlated
+      depth*depth + # wave*we + ## biophysical small-scale
+      nutr*nutrient_load + #productivity*npp +# temperature*temp + ## biophysical large-scale,
+      management[management_rules] + gravity*grav_NC , ## human
     
-    # # ## seascape intercepts, sampling normal from the mean of each country
-    seascape_X[seascape] ~ dnorm(mu_seascape, sigma_seascape),
-    # 
-    # ## now linking site to country
-    # ## country-level intercepts and seascape-level covariates [maybe don't exist?]
-    # # mu_country <- country_X[countryLookup],
-    # 
-    # # ## country intercepts, drawing from global mu
-    # # country_X[countryLookup] ~ dnorm(mu_country, sigma_country),
-    # 
+    # ## ## now linking site to country
+    site_X[site] ~ dnorm(mu_country, sigma_country),
+    mu_country <- country_X[countryLookup],
+
     # ## global intercept, plus country-level covariates [HDI / population size / ???]
-    mu_seascape <- y ,
+    country_X <- y ,
     
-    # 
     # # weak prior on 0
     y ~ dnorm(0,10),
     
-    # seascape_X[seascape] ~ dnorm(0,1),
+    site_X[site] ~ dnorm(0,1),
     
     ## weak prior on continuous
     c(coral,
       ma,
       depth,
-      wave,
+      # wave,
       gravity,
-      sed,
-      nutr,
-      productivity
+      # sed,
+      nutr
     ) ~ dnorm(0, 10),
     
     ## weak prior on categorical
@@ -118,27 +86,24 @@ m_biom <- ulam(
     # reef_ty[reef_type] ~ dnorm(0, 1),
     # reef_zo[reef_zone] ~ dnorm(0, 1),
     management[management_rules] ~ dnorm(0, 1),
-    year[year_fish] ~ dnorm(0, 1),
-    
     
     ## hyper-priors
     sigma ~ dexp(2),
-    sigma_seascape ~ dexp(1) # Variation among seascapes
-    # sigma_country ~ dexp(1) # Variation among country
+    sigma_country ~ dexp(1) # Variation among sites
   ) , 
   data=focal.list , chains=3 , cores=6, control=list(adapt_delta=0.99), warmup=1500, iter=3000, log_lik=TRUE )
 
-dashboard(m_biom)
-precis(m_biom)
-plot(precis(m_biom,2))
+dashboard(m)
+precis(m)
+plot(precis(m,2))
 
 ## compare model predictions
-p.index<-link(m_biom)$mu ## note this returns predictions for mu_small and mu_large too, so only taking $mu here
+p.index<-link(m)$mu ## note this returns predictions for mu_small and mu_large too, so only taking $mu here
 index_mu<-apply( p.index , 2 , median )
 
-plot(log(focal.list$biomass_kgha_avg), index_mu)
+plot(log(focal.list$nutprop), index_mu)
 abline(0, 1)
 
-save(m_biom, fish, focal.list, file = 'results/mod.rds')
+save(m, focal.list, file = 'results/mod.rds')
 
 
