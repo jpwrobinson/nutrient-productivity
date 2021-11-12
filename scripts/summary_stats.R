83419e68e4e5bd3fd749ca55088c600c09f46da7
James Robinson <jpwrobinson@gmail.com>
2021-11-12 15:30:04 +0000

    summ stats and fig2 axcis
    Former-commit-id: c4a7c3e26a94d78abd493dc0f20d94b58eb752d4
    Former-commit-id: 6f590ee8e6808aefa6d6300261fee2c74c4cedf9
    Former-commit-id: f7da072888970b735f55f0930e41f837765505e3

--- scripts/summary_stats.R
+++ scripts/summary_stats.R
@@ -3,9 +3,31 @@ library(tidyverse)
 load(file = 'results/wcs_productivity.rds')
 load(file = 'results/wcs_nut_prod.rds')
 
-
+## UVC metadata
 fishp %>% summarise(n=n_distinct(site))
 fishp %>% group_by(country, year) %>% summarise(n=n_distinct(site))
 fishp %>% group_by(country) %>% summarise(n=n_distinct(site))
 fishp %>% group_by(country, site) %>% summarise(n=n_distinct(year)) %>% 
           group_by(country,n) %>% count()
+
+
+## Figure 1 metadata
+fish2<-fishp %>% group_by(fish_taxon, fish_family, dietP, lmax, nscore,
+                          calcium.mg, iron.mg, selenium.mug, zinc.mg, omega3.g, vitamin_a.mug) %>% 
+  summarise(Kmax =mean(Kmax)) %>% ungroup() 
+
+cor(fish2$Kmax, fish2$nscore)
+fish2 %>% filter(nscore > 350) %>% summarise(mean(Kmax))
+fish2 %>% filter(nscore < 200 & nscore > 90) %>% summarise(range(Kmax))
+fish2 %>% filter(Kmax > 1) %>% distinct(fish_taxon,  fish_family, dietP) %>% data.frame()
+
+## Figure 2 metadata
+source('scripts/fig/Figure2.R')
+prod_fg2 %>% mutate_if(is.numeric, function(x) round(x*100, 0))
+prod_fg %>% mutate_if(is.numeric, function(x) round(x*100, 0)) %>% filter(nutrient=='vitamin_a.mug')
+
+## excluding vitA
+prod_fg %>% filter(nutrient!='vitamin_a.mug') %>%  
+  group_by(dietP_lab) %>% summarise(se = se(nutprop), nutprop = mean(nutprop)) %>% 
+  mutate(lower = nutprop - 2*se, upper = nutprop + 2*se) %>% 
+  mutate_if(is.numeric, function(x) round(x*100, 0))
