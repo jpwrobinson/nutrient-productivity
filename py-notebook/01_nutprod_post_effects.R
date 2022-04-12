b34ad02c1ce7f386aac2aea428fd789a3bcd7952
James Robinson <jpwrobinson@gmail.com>
2022-04-12 13:21:18 -0300

    trace draws save
    Former-commit-id: f6a474873ee2dde31407ba69df4766594959860b [formerly c4c7219032a4211db76bd0205a2afd367e5bed46] [formerly 73e51ecd693f3dd673013192f7e6b7be88f46330] [formerly ea31832c6cdc40fef66c69ed7905b87941fb84d1]
    Former-commit-id: 0b9faf3dff4e18ee2bae7b3f1754251bf0807c62
    Former-commit-id: 9aeb886094217c00e24f1ac680cf53edc5f48305
    Former-commit-id: 9f494a6fa24c667cbb61bf41099243d87f849c32

--- py-notebook/01_nutprod_post_effects.R
+++ py-notebook/01_nutprod_post_effects.R
@@ -2,10 +2,26 @@
 
 posts<-read.csv('py-notebook/zinc_posterior_summary.csv') %>% 
       mutate(var = str_split_fixed(X, '\\[', 2)[,1]) %>% 
-      filter(var != 'intercept')
+      filter(!var %in% c('intercept', 'alpha', 'β0_c')) %>% 
+      mutate(varname = str_split_fixed(varname, '\\[', 2)[,1],
+             varnum=as.numeric(factor(var)))
 
-ggplot(posts, aes(var, mean, ymin = hdi_3., ymax = hdi_97., col=fg)) +
+conts<-c('hard_coral', 'bare_sub', 'gravity', 'turf', 'population', 'macroalgae', 'depth')
+
+g1<-ggplot(posts %>% filter(!varname %in% conts), aes(fct_reorder(varname,varnum), mean, ymin = hdi_2.5., ymax = hdi_97.5., col=fg)) +
       geom_hline(yintercept=0, linetype=5) +
       geom_pointrange(position = position_dodge(width=0.5))  +
       coord_flip() +
       labs(x = '', y = 'Posterior mean')
+
+g2<-ggplot(posts %>% filter(varname%in%conts), 
+           aes(varname, mean, ymin = hdi_2.5., ymax = hdi_97.5., col=fg)) +
+  geom_hline(yintercept=0, linetype=5) +
+  geom_pointrange(position = position_dodge(width=0.5))  +
+  coord_flip() +
+  labs(x = '', y = 'Posterior mean')
+
+pdf(file = 'fig/model/posterior_params.pdf', height=5, width=10)
+print(g1)
+print(g2)
+dev.off()
