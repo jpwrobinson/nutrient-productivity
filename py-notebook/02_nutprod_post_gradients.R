ddbd03ff690bfaaa3a3c3a7a3f0839a7d4139eb4
James Robinson <jpwrobinson@gmail.com>
2022-04-14 13:39:10 -0300

    posterior preds
    Former-commit-id: 69c048180e382b949c41323414055bd03a0f6cf9
    Former-commit-id: 9aa72668d5b3655077ab2f2bf5c3fa10651e59bc

--- py-notebook/02_nutprod_post_gradients.R
+++ py-notebook/02_nutprod_post_gradients.R
@@ -3,7 +3,7 @@ library(tidyverse)
 stdize<-function(x){(x-mean(x))/(2*sd(x))}
 
 focal<-read.csv('py-notebook/zinc.mg_unscaled.csv')
-gravS<-stdize(focal$grav_nc)
+
 
 fgnames<-data.frame(nfg = 0:5, fg = c('herbivore-detritivore',
           'herbivore-macroalgae',
@@ -12,25 +12,57 @@ fgnames<-data.frame(nfg = 0:5, fg = c('herbivore-detritivore',
           'piscivore',
           'planktivore'))
 
-post<-read.csv('py-notebook/zinc_posterior_gravity.csv')
-names(post)<-fgnames$fg
-post$grav_nc<-seq(min(gravS), max(gravS), length.out=100)
-post$grav_nc_raw<-seq(min(focal$grav_nc), max(focal$grav_nc), length.out=100)
-post<-post %>% pivot_longer(-c(grav_nc, grav_nc_raw), names_to = 'fg', values_to = 'mu') 
+covs<-c('gravity',
+        'hard_coral',
+        'macroalgae',
+        'bare_substrate',
+        'turf',
+        'manage')
 
-lo<-read.csv('py-notebook/zinc_posterior_gravity_hpd_lo.csv')
-names(lo)<-fgnames$fg
-lo<-lo %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
+covs2<-c('grav_nc',
+        'hard_coral',
+        'macroalgae',
+        'bare_substrate',
+        'turf_algae',
+        'management_rules')
 
-hi<-read.csv('py-notebook/zinc_posterior_gravity_hpd_hi.csv')
-names(hi)<-fgnames$fg
-hi<-hi %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
+pdf(file = 'fig/model/posterior_predicted.pdf', height=7, width=12)
 
-post$lo<-lo$mu
-post$hi<-hi$mu
+for(i in 1:length(covs)){
+  post<-read.csv(paste0('py-notebook/zinc_posterior_', covs[i],'.csv'))
+  names(post)<-fgnames$fg
+  if(covs[i]!='manage'){
+    S<-stdize(focal[,covs2[i]])
+    post$X<-seq(min(S), max(S), length.out=100)
+    post$X_raw<-seq(min(focal[,covs2[i]]), max(focal[,covs2[i]]), length.out=100)
+  } else {
+    post$X<-c('access restriction',
+              'gear restriction',
+              'no-take',
+              'open-access',
+              'time restriction')
+    post$X_raw<-post$X
+    }
+  
+  post<-post %>% pivot_longer(-c(X, X_raw), names_to = 'fg', values_to = 'mu') 
+  
+  lo<-read.csv(paste0('py-notebook/zinc_posterior_', covs[i],'_hpd_lo.csv'))
+  names(lo)<-fgnames$fg
+  lo<-lo %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
+  
+  hi<-read.csv(paste0('py-notebook/zinc_posterior_', covs[i],'_hpd_hi.csv'))
+  names(hi)<-fgnames$fg
+  hi<-hi %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
+  
+  post$lo<-lo$mu
+  post$hi<-hi$mu
+  
+  g<-ggplot(post, aes(X_raw, mu)) + 
+    geom_ribbon(aes(ymin = lo, ymax = hi, group=fg), alpha=0.5, fill='grey90') +
+    geom_line(aes(col=fg)) +
+    labs(x = covs[i], y = 'Proportion zinc production', subtitle = covs[i]) +
+    facet_wrap(~fg)
+  print(g)
+}
 
-ggplot(post, aes(grav_nc_raw, mu)) + 
-  geom_ribbon(aes(ymin = lo, ymax = hi, group=fg), alpha=0.5, fill='grey90') +
-  geom_line(aes(col=fg)) +
-  labs(x = 'Gravity', y = 'Proportion zinc production') +
-  facet_wrap(~fg)
+dev.off()
