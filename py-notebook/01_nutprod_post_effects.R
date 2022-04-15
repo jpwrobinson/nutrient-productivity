385c89c1af93a1683d961ab2a363f96d4910b81c
James Robinson <jpwrobinson@gmail.com>
2022-04-15 14:54:14 -0300

    mean intercept preds
    Former-commit-id: 76c0cec5daaa580454dab34574db3947db2a2584 [formerly 10ca7abe8aef2141d6de16d8a055f6f796cfa5b2] [formerly 67aad38b4ce597df26e72f3a153d6cd198a1b5fa] [formerly f2348efa1832f99016d37d3bfdc562eabf6c34d0]
    Former-commit-id: ac208b8adfa7c7d4d1924001f19c8bd389624546
    Former-commit-id: 8951016258fe8f774bec9bc30eb05beb278c9acf
    Former-commit-id: 88236533df236ef2939de09d85fd972c84b7b0ba

--- py-notebook/01_nutprod_post_effects.R
+++ py-notebook/01_nutprod_post_effects.R
@@ -6,7 +6,7 @@ posts<-read.csv('py-notebook/zinc_posterior_summary.csv') %>%
       mutate(varname = str_split_fixed(varname, '\\[', 2)[,1],
              varnum=as.numeric(factor(var)))
 
-conts<-c('hard_coral', 'bare_sub', 'gravity', 'turf', 'population', 'macroalgae', 'depth')
+conts<-c('hard_coral', 'bare_sub', 'gravity', 'turf', 'population', 'macroalgae', 'depth', 'sediment', 'nut_load')
 
 g1<-ggplot(posts %>% filter(!varname %in% conts), aes(fct_reorder(varname,varnum), mean, ymin = hdi_2.5., ymax = hdi_97.5., col=fg)) +
       geom_hline(yintercept=0, linetype=5) +
