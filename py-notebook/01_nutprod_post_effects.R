79d15cbb1ff777cea4efd6741fc0d8e8c27ac668
James Robinson <jpwrobinson@gmail.com>
2022-04-19 14:44:38 -0400

    trace csv with lfs
    Former-commit-id: 122041595ae0fa016b6402b2c7fd0a109ffe0031 [formerly d2eb4d05de120a5aeacce4223c758f595f0e82ef] [formerly 526ede429e8eb73fedbe796c2ed0fe61538aded3]
    Former-commit-id: 137694ec03206e74880b73819bc6cd088d9ae507
    Former-commit-id: b01490b3148749d9cdf5bacb20be6ba3ca070289

--- py-notebook/01_nutprod_post_effects.R
+++ py-notebook/01_nutprod_post_effects.R
@@ -25,3 +25,30 @@ pdf(file = 'fig/model/posterior_params.pdf', height=5, width=10)
 print(g1)
 print(g2)
 dev.off()
+
+
+
+
+posts<-read.csv('py-notebook/prod_posterior_summary.csv') %>% 
+  mutate(var = str_split_fixed(X, '\\[', 2)[,1]) %>% 
+  filter(!var %in% c('intercept', 'alpha', 'β0_cnc', 'β0_managenc')) %>% 
+  mutate(varname = str_split_fixed(varname, '\\[', 2)[,1],
+         varnum=as.numeric(factor(var)))
+
+conts<-c('hard_coral', 'bare_sub', 'gravity', 'turf', 'population', 'macroalgae', 'depth', 'sediment', 'nut_load')
+
+g1<-ggplot(posts %>% filter(!varname %in% conts), aes(fct_reorder(varname,varnum), mean, ymin = hdi_2.5., ymax = hdi_97.5., col=fg)) +
+  geom_hline(yintercept=0, linetype=5) +
+  geom_pointrange(position = position_dodge(width=0.5))  +
+  coord_flip() +
+  labs(x = '', y = 'Posterior mean')
+
+g2<-ggplot(posts %>% filter(varname%in%conts), 
+           aes(varname, mean, ymin = hdi_2.5., ymax = hdi_97.5., col=fg)) +
+  geom_hline(yintercept=0, linetype=5) +
+  geom_pointrange(position = position_dodge(width=0.5))  +
+  coord_flip() +
+  labs(x = '', y = 'Posterior mean')
+
+g1
+g2
