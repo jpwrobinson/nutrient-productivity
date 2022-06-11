29f9dfa4e8f505e4cb2319404d30ae71048b718e
James Robinson <jpwrobinson@gmail.com>
2022-06-11 16:38:05 +0100

    posterior pred plots
    Former-commit-id: 3e068091028f4c12a260f272a211e72fb5084571 [formerly af2e6ed731a27da270ba9b1280b6b306c49ee3e6] [formerly 772e49fbe6381ab87d4c5982c931da3bb9e4d84d]
    Former-commit-id: f57214f197c81d2319a8feb429c34ef24f55fe52
    Former-commit-id: 6af4678219302b079fa4dcce160420fa2f6c7ffb

--- py-notebook/02_nutprod_post_gradients.R
+++ py-notebook/02_nutprod_post_gradients.R
@@ -1,95 +1,19 @@
-library(tidyverse)
+## Plot predicted covariate values
 
-stdize<-function(x){(x-mean(x))/(2*sd(x))}
+## prod
+focal<-read.csv('py-notebook/zinc.mg_reef_unscaled.csv')
+filename='py-notebook/prod/'
+figname='fig/model/posterior_predicted_productivity.pdf'
+source('py-notebook/func_posterior_pred_plot.R')
 
-focal<-read.csv('py-notebook/zinc.mg_unscaled.csv')
+## biomass
+focal<-read.csv('py-notebook/zinc.mg_reef_unscaled.csv')
+filename='py-notebook/biomass/'
+figname='fig/model/posterior_predicted_biomass.pdf'
+source('py-notebook/func_posterior_pred_plot.R')
 
-
-fgnames<-data.frame(nfg = 0:6, fg = c('browser',
-                                      'cropper/grazer',
-                                      'invertivore-mobile',
-                                      'scraper-excavator',
-                                      'piscivore',
-                                      'planktivore',
-                                      'mixed-diet feeder'))
-
-covs<-c('gravity',
-        'hard_coral',
-        'macroalgae',
-        'bare_substrate',
-        'turf',
-        'manage',
-        'pop',
-        'sediment',
-        'nut_load')
-
-covs2<-c('grav_nc',
-        'hard_coral',
-        'macroalgae',
-        'bare_substrate',
-        'turf_algae',
-        'management_rules',
-        'pop_count', 
-        'sediment', 
-        'nutrient_load')
-
-# managecovs<-c('Belizegear restriction',
-#               'Belizeno-take',
-#               'Fijiaccess restriction',
-#               'Fijino-take',
-#               'Fijitime restriction',
-#               'Madagascargear restriction',
-#               'Madagascarno-take',
-#               'Madagascaropen-access',
-#               'Solomon Islandsaccess restriction',
-#               'Solomon Islandsno-take',
-#               'Solomon Islandstime restriction')
-
-managecovs<-c('Belizerestriction',
-              'Belizeno-take',
-              'Fijirestriction',
-              'Fijino-take',
-              'Madagascarrestriction',
-              'Madagascarno-take',
-              'Madagascaropen-access',
-              'Solomon Islandsrestriction',
-              'Solomon Islandsno-take')
-
-pdf(file = 'fig/model/posterior_predicted.pdf', height=5, width=12)
-
-for(i in 1:length(covs)){
-  post<-read.csv(paste0('py-notebook/zinc_posterior_', covs[i],'.csv'))
-  names(post)<-fgnames$fg
-  if(covs[i]!='manage'){
-    S<-stdize(focal[,covs2[i]])
-    post$X<-seq(min(S), max(S), length.out=100)
-    post$X_raw<-seq(min(focal[,covs2[i]]), max(focal[,covs2[i]]), length.out=100)
-  } else {
-    post$X<-managecovs
-    post$X_raw<-post$X
-    }
-  
-  post<-post %>% pivot_longer(-c(X, X_raw), names_to = 'fg', values_to = 'mu') 
-  
-  lo<-read.csv(paste0('py-notebook/zinc_posterior_', covs[i],'_hpd_lo.csv'))
-  names(lo)<-fgnames$fg
-  lo<-lo %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
-  
-  hi<-read.csv(paste0('py-notebook/zinc_posterior_', covs[i],'_hpd_hi.csv'))
-  names(hi)<-fgnames$fg
-  hi<-hi %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
-  
-  post$lo<-lo$mu
-  post$hi<-hi$mu
-  
-  g<-ggplot(post, aes(X_raw, mu)) + 
-    labs(x = covs[i], y = 'Proportion zinc production', subtitle = covs[i]) +
-    facet_grid(~fg)
-  
-  if(covs[i]!='manage'){print(g + geom_ribbon(aes(ymin = lo, ymax = hi, group=fg), alpha=0.5, fill='grey90') +
-          geom_line(aes(col=fg)))} else {
-            print(g + geom_pointrange(aes(ymin = lo, ymax = hi, col=X)))
-          }
-}
-
-dev.off()
+## turnover
+focal<-read.csv('py-notebook/zinc.mg_reef_unscaled.csv')
+filename='py-notebook/turnover/'
+figname='fig/model/posterior_predicted_turnover.pdf'
+source('py-notebook/func_posterior_pred_plot.R')
