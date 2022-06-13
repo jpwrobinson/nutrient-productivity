f4eec158783c7943117f912c1d4ac1a465e015c5
James Robinson <jpwrobinson@gmail.com>
2022-06-13 08:37:22 +0100

    biom mod final
    Former-commit-id: 86f2db8ccb980ba95619b6d9a38766666d2011d5 [formerly 30fde86f38a22cc2ecace8e639e556549a3f3369] [formerly 7aebfb3a031871ef7d25c118664f477cf4c422e2]
    Former-commit-id: a062228e301c27d7b75adf1bb6368164c6595c22
    Former-commit-id: be45966ab298b61317877f972de5d806fc9c9d0d

--- py-notebook/func_posterior_pred_plot.R
+++ py-notebook/func_posterior_pred_plot.R
@@ -16,9 +16,11 @@ covs<-c('gravity',
         'bare_substrate',
         'turf',
         'manage',
+        # 'country',
         'pop',
         'sediment',
-        'nut_load')
+        'nut_load',
+        'rubble')
 
 covs2<-c('grav_nc',
          'hard_coral',
@@ -26,21 +28,12 @@ covs2<-c('grav_nc',
          'bare_substrate',
          'turf_algae',
          'management_rules',
+         # 'country',
          'pop_count', 
          'sediment', 
-         'nutrient_load')
+         'nutrient_load',
+         'rubble')
 
-# managecovs<-c('Belizegear restriction',
-#               'Belizeno-take',
-#               'Fijiaccess restriction',
-#               'Fijino-take',
-#               'Fijitime restriction',
-#               'Madagascargear restriction',
-#               'Madagascarno-take',
-#               'Madagascaropen-access',
-#               'Solomon Islandsaccess restriction',
-#               'Solomon Islandsno-take',
-#               'Solomon Islandstime restriction')
 
 managecovs<-c('Belizerestriction',
               'Belizeno-take',
@@ -52,41 +45,43 @@ managecovs<-c('Belizerestriction',
               'Solomon Islandsrestriction',
               'Solomon Islandsno-take')
 
+countrycovs<-c('Belize','Fiji', 'Madgascar', 'Solomon Islands')
+
 pdf(file = figname, height=5, width=12)
 
 for(i in 1:length(covs)){
   post<-read.csv(paste0(filename, 'posterior_', covs[i],'.csv'))
   # names(post)<-fgnames$fg
   names(post) = 'mu'
-  if(covs[i]!='manage'){
+  post$mu <- post$mu
+  
+  if(!covs[i] %in% c('manage', 'country')){
     S<-stdize(focal[,covs2[i]])
     post$X<-seq(min(S), max(S), length.out=100)
     post$X_raw<-seq(min(focal[,covs2[i]]), max(focal[,covs2[i]]), length.out=100)
-  } else {
+  } else if (covs[i] == 'manage'){
     post$X<-managecovs
     post$X_raw<-post$X
+  } else if (covs[i] == 'country') {
+    post$X<-countrycovs
+    post$X_raw<-post$X
   }
-  
-  # post<-post %>% pivot_longer(-c(X, X_raw), names_to = 'fg', values_to = 'mu')
-  
+
+  ## read HPDI  
   lo<-read.csv(paste0(filename, 'posterior_', covs[i],'_hpd_lo.csv'))
-  # names(lo)<-fgnames$fg
   names(lo)<-'mu'
-  # lo<-lo %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
   
   hi<-read.csv(paste0(filename, 'posterior_', covs[i],'_hpd_hi.csv'))
-  # names(hi)<-fgnames$fg
   names(hi)<-'mu'
-  # hi<-hi %>% pivot_longer(cols=everything(), names_to = 'fg', values_to = 'mu') 
   
   post$lo<-lo$mu
   post$hi<-hi$mu
   
   g<-ggplot(post, aes(X_raw, mu)) + 
     labs(x = covs[i], y = 'Posterior predicted', subtitle = covs[i])
-    # facet_grid(~fg)
   
-  if(covs[i]!='manage'){print(g + geom_ribbon(aes(ymin = lo, ymax = hi), alpha=0.5, fill='grey90') +
+  if(!covs[i] %in% c('manage', 'country')){
+    print(g + geom_ribbon(aes(ymin = lo, ymax = hi), alpha=0.5, fill='grey90') +
                                 geom_line())} else {
                                   print(g + geom_pointrange(aes(ymin = lo, ymax = hi, col=X)))
                                 }
