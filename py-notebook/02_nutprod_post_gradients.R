55782c440e5e41ae27fcacad8d0f1c0b31349cea
jpwrobinson <jpwrobinson@gmail.com>
2022-06-13 15:58:24 +0100

    new posterior futre scenario reads
    Former-commit-id: e4391f95a008d5fc868a16d3be80be13080a08bf
    Former-commit-id: 5dc02873e9de646f82e858738f9df429210c2843

--- py-notebook/02_nutprod_post_gradients.R
+++ py-notebook/02_nutprod_post_gradients.R
@@ -17,3 +17,36 @@ focal<-read.csv('py-notebook/zinc.mg_reef_unscaled.csv')
 filename='py-notebook/turnover/'
 figname='fig/model/posterior_predicted_turnover.pdf'
 source('py-notebook/func_posterior_pred_plot.R')
+
+### Future scenarios
+figname='fig/model/future_biomass.pdf'
+filename='py-notebook/biomass/'
+var_name = 'biomass, kg ha'
+source('py-notebook/func_posterior_futures.R')
+
+figname='fig/model/future_productivity.pdf'
+filename='/prod'
+var_name = 'productivity, g day ha'
+source('py-notebook/func_posterior_futures.R')
+
+
+## Future scenarios holding background conditions
+figname='fig/model/future_biomass_background.pdf'
+filename='/biomass'
+var_name = 'biomass, kg ha'
+source('py-notebook/func_posterior_read.R')
+
+figname='fig/model/future_productivity_background.pdf'
+filename='/prod'
+var_name = 'productivity, g day ha'
+source('py-notebook/func_posterior_read.R')
+
+
+
+
+## testing brms alt
+# library(brms)
+# focal$ylog<-log(focal$biomass_kgha)
+# f1<-brm(ylog ~ grav_nc + hard_coral + macroalgae + bare_substrate +
+#           turf_algae + rubble + pop_count + depth + management_rules, data=focal)
+# summary(f1)
--- scripts/fig/FigureSX_benthic_cov.R
+++ scripts/fig/FigureSX_benthic_cov.R
@@ -13,7 +13,7 @@ load('data/wcs/wcs_fish_benthic.rds')
 ben<-fish_avg %>% filter(country %in% c('Belize', 'Fiji', 'Madagascar', 'Solomon Islands')) %>% 
       ungroup() %>% 
       mutate(algae = macroalgae + turf_algae) %>% 
-      select(site, country, hard_coral, macroalgae, bare_substrate, turf_algae, algae) %>% 
+      select(site, country, hard_coral, macroalgae, bare_substrate, rubble, turf_algae, algae) %>% 
       pivot_longer(-c(site, country, hard_coral), names_to = 'sub', values_to = 'cover') %>% 
       mutate(id = paste0(country, sub)) %>% 
       filter(site != 'NK02') %>% 
