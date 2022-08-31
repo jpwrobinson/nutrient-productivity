d4d321f9462d7bdaa7d19d748585e4e5195ed408
jpwrobinson <jpwrobinson@gmail.com>
2022-08-31 11:00:32 +0100

    posterior fig
    Former-commit-id: b3d940c82cf116809d40bdd2ce401e3fc58434ef [formerly 7a853fa9583b46980152193ebf09fecb2b281911] [formerly 22cbd1ca0fe8cd462cbf395708637dccf4bacce8]
    Former-commit-id: 94003f953f4228a171d6234a4897cfc137d1e302
    Former-commit-id: 23ce9cc55c02586dd07c353616844e24925dc5f5

---
+++ scripts/fig/FigureSX_service_posts.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+source('scripts/0_plot_theme.R')
+
+conts<-c('hard_coral', 'bare_sub', 'gravity', 'turf', 'population', 'macroalgae', 'sediment', 'nut_load')
+
+pBiom<-read.csv('py-notebook/biomass/posterior_summary.csv') %>% mutate(service = 'Biomass')
+pProd<-read.csv('py-notebook/prod/posterior_summary.csv') %>% mutate(service = 'Productivity')
+pTurn<-read.csv('py-notebook/turnover/zinc_posterior_summary.csv') %>% mutate(service = 'Zinc turnover')
+pDens<-read.csv('py-notebook/density/posterior_summary.csv') %>% mutate(service = 'Nutrient density')
+
+# join posts
+posts<-rbind(pBiom, pProd, pTurn, pDens) %>% 
+  mutate(var = str_split_fixed(X, '\\[', 2)[,1]) %>% 
+  # filter(!var %in% c('intercept', 'alpha', 'β0_cnc', 'β0_managenc')) %>%
+  filter(var %in% conts) %>%
+  mutate(varname = str_split_fixed(varname, '\\[', 2)[,1],
+         varnum=as.numeric(factor(var)),
+         fill = ifelse(hdi_2.5. > 0 & hdi_97.5. > 0, 'darkgreen', 'grey'),
+         fill = ifelse(hdi_2.5. < 0 & hdi_97.5. < 0, 'darkred', fill))
+
+levs<-c('hard_coral','macroalgae', 'bare_sub', 'turf',  'gravity', 'population', 'sediment', 'nut_load')
+labs<-c('Hard coral','Macroalgae', 'Bare substrate', 'Turf algae',  'Market gravity', 'Human population', 'Sedimentation', 'Nutrient load')
+posts$varname<-factor(posts$varname, levels=rev(levs))
+
+g1<-ggplot(posts, aes(mean, varname, xmin = hdi_2.5., xmax = hdi_97.5., fill=fill)) + 
+      geom_vline(xintercept = 0, linetype=5) +
+      geom_pointrange(pch=21, fatten=8) + 
+      scale_fill_identity() +
+      scale_y_discrete(labels=rev(labs)) +
+      labs(x = 'posterior', y = '') +
+      facet_grid(~service) +
+      theme(panel.grid.minor.x = element_line(colour='grey')) +
+      th 
+
+pdf(file = 'fig/FigureSX_posterior_services.pdf', height =7, width=12)
+print(g1)
+dev.off()
